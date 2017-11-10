edcf47e1e8a86ad0bbb3f1bcd1d8aa225aaf4c75
Jonas L <joaohccl@live.com.pt>
2017-11-10 22:03:12 +0000

    orderHistory

--- PrimaveraWeb/public/js/controllers/ProfileCtrl.js
+++ PrimaveraWeb/public/js/controllers/ProfileCtrl.js
@@ -13,6 +13,7 @@ angular.module('ProfileCtrl', []).controller('ProfileController', function ($sco
                 $scope.showWishList = false;
                 $scope.showHistory = false;
                 $scope.showOrders = false;
+                $scope.historyList = [];
 
                 document.getElementById('overview').classList.remove('active');
                 document.getElementById('wishlist').classList.remove('active');
@@ -30,9 +31,14 @@ angular.module('ProfileCtrl', []).controller('ProfileController', function ($sco
                         document.getElementById('wishlist').classList.add('active');
 
                 } else if (val == 'history') {
-                        $scope.content = "history content";
                         $scope.showHistory = true;
                         document.getElementById('history').classList.add('active');
+                        // TODO fazer get e atualizar este ciclo manhoso
+                        $scope.totalOrders = 3;
+                        for(var i = 0; i < 3; i++){
+                            var products = [{id: 3, name: 'Darkside',price: '9.99'},{id: 47, name: 'Nonagon Infinity', price: '0.10'}];
+                            $scope.historyList.push({orderId: 80+i,orderPrice: '10.09', date: '19/9/2019', products: products});
+                        }
 
                 } else if (val == 'orders') {
                         $scope.content = "orders content";
