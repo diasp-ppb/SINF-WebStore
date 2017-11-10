7eb7ae7892be6af4568fc4d1feb7312388a34458
Jonas L <joaohccl@live.com.pt>
2017-11-10 03:58:43 +0000

    Auto stash before merge of "master" and "origin/master"

--- PrimaveraWeb/public/js/controllers/ProductCtrl.js
+++ PrimaveraWeb/public/js/controllers/ProductCtrl.js
@@ -1,5 +1,5 @@
 angular.module('ProductCtrl', []).controller('ProductController', function($scope) {
-
+    $scope.$parent.products = {category: ['rock', 'hip pop', 'eletronic', 'jazz', 'v4p0r w4v3']};
     $scope.tagline = 'The square root of life is pi!';
-
+    $scope.$parent.productP = true;
 });
