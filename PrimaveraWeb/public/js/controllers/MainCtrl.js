260c56e77f4c4742fb04619ac5c9b4705a33dbbf
rodavoce <up201404178@fe.up.pt>
2017-11-11 16:46:35 +0000

    http request working

--- PrimaveraWeb/public/js/controllers/MainCtrl.js
+++ PrimaveraWeb/public/js/controllers/MainCtrl.js
@@ -1,15 +1,14 @@
 angular.module('MainCtrl', []).controller('MainController', function($scope,$http) {
 
-    $http.get("http://localhost:8080", {
+    $http.get("http://localhost:49822/api/artigos", {
         headers: {
             "content-type" : "application/json"
         }
-    }).then(function(response) {
-        // Request completed successfully
+    }).then(function (response) {
         console.log(response);
+        $scope.products = response.data
     }, function (x) {
         console.log(x);
     });
-
     
 });
