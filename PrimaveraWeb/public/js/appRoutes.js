37dbbe7920bf3c0731636628b89f895edd3a8eb1
Bruno <pandasarehere@gmail.com>
2017-12-12 00:40:45 +0000

    Removing trash (pedro dias)

--- PrimaveraWeb/public/js/appRoutes.js
+++ PrimaveraWeb/public/js/appRoutes.js
@@ -7,17 +7,6 @@ angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', f
 			templateUrl: 'views/home.html',
 			controller: 'MainController'
 		})
-
-		.when('/nerds', {
-			templateUrl: 'views/nerd.html',
-			controller: 'NerdController'
-		})
-
-		.when('/geeks', {
-			templateUrl: 'views/geek.html',
-			controller: 'GeekController'	
-		})
-
 		.when('/signin', {
 			templateUrl: 'views/signin.html',
 			controller: 'NavBarController'	
