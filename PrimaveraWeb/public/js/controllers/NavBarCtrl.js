84a18ff89cade098da2ccde6fba47f27bd56d954
Bruno <pandasarehere@gmail.com>
2017-12-13 05:31:45 +0000

    lalala

--- PrimaveraWeb/public/js/controllers/NavBarCtrl.js
+++ PrimaveraWeb/public/js/controllers/NavBarCtrl.js
@@ -1,7 +1,9 @@
 angular.module('NavBarCtrl', []).controller('NavBarController', function ($scope, $http, $window, $cookies) {
 
 
-    $scope.loggedUser = $cookies.get('newCookie');
+    if(typeof $scope.logged !== "undefined"){
+        $scope.loggedUser = $cookies.get('newCookie');
+    }
     console.log($scope.logged);
     console.log($scope.loggedUser);
 
